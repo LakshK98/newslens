a09f09bc9e7e0951cdb6f4fcac2ec3705400713b
Laksh Kotian <lakshkotian98@gmail.com>
2023-08-06 20:18:11 -0400

    Adding CORS

--- src/main/java/com/newslens/newslens/FirstController.java
+++ src/main/java/com/newslens/newslens/FirstController.java
@@ -1,9 +1,11 @@
 package com.newslens.newslens;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class FirstController {
     @GetMapping("/")
     public String index() {
